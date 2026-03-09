c19254b492c4a86198c2368c29b2d3710e50e0fd
Bautista Heyde <bautistaheyde@hotmail.com>
2026-03-09 12:40:33 -0300

    run

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000',
+      'http://localhost:3001',
       'https://moro-abogados.vercel.app',
       'https://www.moroabogados.com.ar'
     ],
